c2bbb2af49612ccaf0ef56c0d19334b7a6eea0ab
danifc123 <danielcarmo29@gmail.com>
2025-11-17 21:43:07 -0300

    style: navbar pronta

--- backend/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
+++ backend/Models/obj/Debug/net9.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037c411c0699637c82851b77fcfa931626354f3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f826f08045d4fa139f4b684496ed315fbbc51515")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  // Estado do menu mobile
+  protected menuOpen = signal(false);
 
+  /**
+   * Alterna o estado do menu mobile
+   */
+  toggleMenu(): void {
+    this.menuOpen.update(value => !value);
+  }
+
+  /**
+   * Fecha o menu mobile ao clicar em um link
+   */
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
+
+  /**
+   * Ação de login
+   */
+  onLogin(): void {
+    // TODO: Implementar navegação para página de login
+    console.log('Navegando para login...');
+    this.closeMenu();
+  }
 }
